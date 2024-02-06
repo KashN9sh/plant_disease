25859d7ddd7b98c4bae6184d62acca9762110fe1
KorolevRoman228 <kash244@yandex.ru>
2024-02-07 02:13:32 +0300

    start files processing

--- back/main.py
+++ back/main.py
@@ -1,7 +1,24 @@
-from fastapi import FastAPI
+from typing import Annotated
+
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
+
+@app.post("/files/")
+async def create_files(files: Annotated[list[bytes], File()]):
+    return {"file_sizes": [len(file) for file in files]}
+
+
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+async def main():
+    content = """
+        <body>
+        <form action="/files/" enctype="multipart/form-data" method="post">
+        <input name="files" type="file" multiple>
+        <input type="submit">
+        </form>
+        </body>
+    """
+    return HTMLResponse(content=content)
