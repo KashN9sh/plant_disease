29aab6006a7facc7d5bda97a6e397d5005b56640
KorolevRoman228 <kash244@yandex.ru>
2024-02-07 03:02:31 +0300

    sm fixes

--- front/plant/src/App.js
+++ front/plant/src/App.js
@@ -24,25 +24,17 @@ function App() {
 
   return (
     <div className="App">
-      <head>
-        <meta charset="utf-8"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1"/>
-        <title>Bootstrap demo</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous"/>
-      </head>
-      <body>
-        <div className="container text-center">
-          <h1>Приветики!</h1>
-          <h2>( ˙꒳​˙ )</h2>
+      
+      <div className="container text-center">
+        <h1>Приветики!</h1>
+        <h2>( ˙꒳​˙ )</h2>
 
-          <div class="mb-3">
-            <input className="form-control" type="file" id="formFile" onChange={handleFileChange}/>
-          </div>
-
-          <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-        
+        <div className="mb-3">
+          <input className="form-control" type="file" id="formFile" onChange={handleFileChange}/>
         </div>
-      </body>
+
+      
+      </div>
     </div>
   );
 }
