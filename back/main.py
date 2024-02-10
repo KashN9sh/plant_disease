2f228e51669fdd3fc013eec120cb32871ba505e4
KorolevRoman228 <kash244@yandex.ru>
2024-02-10 16:59:11 +0300

    predict

--- back/main.py
+++ back/main.py
@@ -3,11 +3,47 @@ from typing import Annotated
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import HTMLResponse
 
+import torch  
+import torchvision.transforms as transforms 
+from PIL import Image
+import io
+
+from model import ResNet9
+from utils import predict_image
+
+
 app = FastAPI()
+
+@app.on_event("startup")
+async def startup_event():
+    """
+    Initialize FastAPI and add variables
+    """
+    # Initialize the pytorch model
+    import __main__
+    setattr(__main__, "ResNet9", ResNet9)
+    model = torch.load('plant-disease-model-complete.pth', map_location=torch.device('cpu'))
+
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Resize((256,256))
+    ])
+    print(model)
+
+    # add model and other preprocess tools too app state
+    app.package = {
+        "transform": transform,  # joblib.load
+        "model": model
+    }
 
 
 @app.post("/files/")
 async def create_files(files: Annotated[list[bytes], File()]):
+    image_data = files[0]
+    image = Image.open(io.BytesIO(image_data))
+    img_tensor = app.package["transform"](image)
+
+    print(predict_image(img_tensor, app.package["model"]))
     return {"file_sizes": [len(file) for file in files]}
 
 
---
+++ back/utils.py
@@ -0,0 +1,23 @@
+from constants import labels
+import torch
+
+# for moving data to device (CPU or GPU)
+def to_device(data, device):
+    """Move tensor(s) to chosen device"""
+    if isinstance(data, (list,tuple)):
+        return [to_device(x, device) for x in data]
+    return data.to(device, non_blocking=True)
+
+def predict_image(img, model):
+    """Converts image to array and return the predicted class
+        with highest probability"""
+    # Convert to a batch of 1
+    xb = to_device(img.unsqueeze(0), torch.device("cpu"))
+    # Get predictions from model
+    yb = model(xb)
+    print(yb)
+    # Pick index with highest probability
+    _, preds  = torch.max(yb, dim=1)
+    # Retrieve the class label
+
+    return labels[preds[0].item()]
