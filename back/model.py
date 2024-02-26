3744ad5312b03cadf08751158770139c096c582c
Kirill Fedorov <k.a.fedorov@gmail.com>
2024-02-26 23:22:10 +0000

    add pre-commit, change deploy, change structure, fix style errors

--- back/model.py
+++ back/model.py
@@ -3,29 +3,42 @@ import torch.nn as nn  # for creating  neural networks
 import torch.nn.functional as F  # for functions for calculating loss
 
 
-# for calculating the accuracy
-def accuracy(outputs, labels):
-    _, preds = torch.max(outputs, dim=1)
-    return torch.tensor(torch.sum(preds == labels).item() / len(preds))
-
-
-# base class for the model
 class ImageClassificationBase(nn.Module):
+    """
+    Base class for the model
+    """
 
-    def training_step(self, batch):
+    @staticmethod
+    def accuracy(outputs, labels) -> torch.Tensor:
+        """
+        Calculate accuracy of the model
+        """
+        _, preds = torch.max(outputs, dim=1)
+        return torch.tensor(torch.sum(preds == labels).item() / len(preds))
+
+    def training_step(self, batch) -> torch.Tensor:
+        """
+        Training step for the model
+
+        Args:
+            batch: batch of data
+
+        Returns:
+            loss: loss of the model
+        """
         images, labels = batch
         out = self(images)  # Generate predictions
         loss = F.cross_entropy(out, labels)  # Calculate loss
         return loss
 
-    def validation_step(self, batch):
+    def validation_step(self, batch) -> dict[str, torch.Tensor]:
         images, labels = batch
         out = self(images)  # Generate prediction
         loss = F.cross_entropy(out, labels)  # Calculate loss
-        acc = accuracy(out, labels)  # Calculate accuracy
+        acc = self.accuracy(out, labels)  # Calculate accuracy
         return {"val_loss": loss.detach(), "val_accuracy": acc}
 
-    def validation_epoch_end(self, outputs):
+    def validation_epoch_end(self, outputs) -> dict[str, torch.Tensor]:
         batch_losses = [x["val_loss"] for x in outputs]
         batch_accuracy = [x["val_accuracy"] for x in outputs]
         epoch_loss = torch.stack(batch_losses).mean()  # Combine loss
@@ -35,7 +48,7 @@ class ImageClassificationBase(nn.Module):
             "val_accuracy": epoch_accuracy,
         }  # Combine accuracies
 
-    def epoch_end(self, epoch, result):
+    def epoch_end(self, epoch, result) -> None:
         print(
             "Epoch [{}], last_lr: {:.5f}, train_loss: {:.4f}, val_loss: {:.4f}, val_acc: {:.4f}".format(
                 epoch,
@@ -50,8 +63,11 @@ class ImageClassificationBase(nn.Module):
 # Architecture for training
 
 
-# convolution block with BatchNormalization
-def ConvBlock(in_channels, out_channels, pool=False):
+def conv_block(in_channels, out_channels, pool=False) -> nn.Sequential:
+    """
+    Convolutional block with BatchNormalization
+    """
+
     layers = [
         nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
         nn.BatchNorm2d(out_channels),
@@ -62,22 +78,23 @@ def ConvBlock(in_channels, out_channels, pool=False):
     return nn.Sequential(*layers)
 
 
-# resnet architecture
 class ResNet9(ImageClassificationBase):
+    """
+    ResNet9 architecture
+    """
+
     def __init__(self, in_channels, num_diseases):
         super().__init__()
 
-        self.conv1 = ConvBlock(in_channels, 64)
-        self.conv2 = ConvBlock(64, 128, pool=True)  # out_dim : 128 x 64 x 64
-        self.res1 = nn.Sequential(ConvBlock(128, 128), ConvBlock(128, 128))
+        self.conv1 = conv_block(in_channels, 64)
+        self.conv2 = conv_block(64, 128, pool=True)  # out_dim : 128 x 64 x 64
+        self.res1 = nn.Sequential(conv_block(128, 128), conv_block(128, 128))
 
-        self.conv3 = ConvBlock(128, 256, pool=True)  # out_dim : 256 x 16 x 16
-        self.conv4 = ConvBlock(256, 512, pool=True)  # out_dim : 512 x 4 x 44
-        self.res2 = nn.Sequential(ConvBlock(512, 512), ConvBlock(512, 512))
+        self.conv3 = conv_block(128, 256, pool=True)  # out_dim : 256 x 16 x 16
+        self.conv4 = conv_block(256, 512, pool=True)  # out_dim : 512 x 4 x 44
+        self.res2 = nn.Sequential(conv_block(512, 512), conv_block(512, 512))
 
-        self.classifier = nn.Sequential(
-            nn.MaxPool2d(4), nn.Flatten(), nn.Linear(512, num_diseases)
-        )
+        self.classifier = nn.Sequential(nn.MaxPool2d(4), nn.Flatten(), nn.Linear(512, num_diseases))
 
     def forward(self, xb):  # xb is the loaded batch
         out = self.conv1(xb)
--- back/utils.py
+++ back/utils.py
@@ -1,10 +1,9 @@
-from constants import labels, descriptions
 import torch
+from constants import descriptions, labels
 
 
-# for moving data to device (CPU or GPU)
 def to_device(data, device):
-    """Move tensor(s) to chosen device"""
+    """Move tensor(s) to chosen device (CPU or GPU)"""
     if isinstance(data, (list, tuple)):
         return [to_device(x, device) for x in data]
     return data.to(device, non_blocking=True)
@@ -12,16 +11,15 @@ def to_device(data, device):
 
 def predict_image(img, model):
     """Converts image to array and return the predicted class
-    with highest probability"""
+    with the highest probability"""
     # Convert to a batch of 1
     xb = to_device(img.unsqueeze(0), torch.device("cpu"))
     output = model(xb)
     probs = torch.nn.functional.softmax(output, dim=1)
     conf, preds = torch.max(probs, dim=1)
-    print(conf, preds)
     # Get predictions from model
     # yb = model(xb)
-    # # Pick index with highest probability
+    # # Pick index with the highest probability
     # _, preds = torch.max(yb, dim=1)
     # print(_, preds)
     # Retrieve the class label
